d10b736c60698f8b49d83d92702d01d2fa047c98
moirir <moirir0906@gmail.com>
2021-11-02 21:02:29 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,10 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :scheduled_delivery_id, numericality: { other_than: 1 }
 end
