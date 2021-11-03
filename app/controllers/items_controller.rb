1f883399d6bb27299c32894ce9ac4f6d7cd104bd
moirir <moirir0906@gmail.com>
2021-11-03 19:42:09 +0900

    商品情報の登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,30 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters'}
+  end
+
+  validates :price, 
+            numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
   has_one_attached :image
   belongs_to :category
   belongs_to :condition
-  belongs_to :delivery_fee
   belongs_to :prefecture
+  belongs_to :delivery_fee
   belongs_to :scheduled_delivery
   belongs_to :user
 
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :delivery_fee_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :scheduled_delivery_id, numericality: { other_than: 1 }
 end
