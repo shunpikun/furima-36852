c65f1aae5787ab4ae265547195eb5198144fb248
moirir <moirir0906@gmail.com>
2021-11-10 21:16:19 +0900

    自身が出品していない商品編集画面への遷移ができない実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_action, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -39,5 +39,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
                                  :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_action
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,12 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :price,
-              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                              message: 'is out of setting range' }
+    validates :price
   end
 
+  validates :price,
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                              message: 'is out of setting range' }
   validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
