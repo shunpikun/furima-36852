177c3016544fe4bf707fea0cf66422879b869674
moirir <moirir0906@gmail.com>
2021-11-10 09:42:35 +0900

    ビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,9 +27,8 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    item.update(item.params)
-    if @item.update(item.params)
-      redirect_to item_path
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
