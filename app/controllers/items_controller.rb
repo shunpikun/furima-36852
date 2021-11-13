f9641fbd143f59a994817d198614179107258a69
moirir <moirir0906@gmail.com>
2021-11-13 11:03:43 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,11 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    end
+    redirect_to root_path if @item.destroy
   end
-
 
   private
 
