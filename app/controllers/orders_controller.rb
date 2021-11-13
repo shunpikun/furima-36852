9a04858b6ff2b5e2739cb8157d9b8ff5dd56d1c3
moirir <moirir0906@gmail.com>
2021-11-13 15:20:32 +0900

    購入のためのformの実装と情報の保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,5 +4,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
-  include Active::Model
-  atter_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order, :user_id, :item_id
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -16,4 +16,5 @@ class OrderAddress
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
