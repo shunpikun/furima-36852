d3a0105bff0290aed0b1492aac9ca9b6eebd417a
moirir <moirir0906@gmail.com>
2021-11-17 21:27:56 +0900

    データベースリセット

--- db/migrate/20211113071211_rename_prefecture_column_to_addresses.rb
+++
@@ -1,5 +0,0 @@
-class RenamePrefectureColumnToAddresses < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :addresses, :prefecture, :prefecture_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_071211) do
+ActiveRecord::Schema.define(version: 2021_11_13_030642) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
