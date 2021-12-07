782f55946cfda5318816f7a67453c588584ad272
moirir <moirir0906@gmail.com>
2021-12-07 21:05:08 +0900

    投稿画像のビューの編集とバリデーションの設定

--- app/models/item_form.rb
+++ app/models/item_form.rb
@@ -8,7 +8,7 @@ class ItemForm
   )
 
   with_options presence: true do
-    validates :images
+    validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
     validates :name
     validates :info
     validates :price
